ec5aa8e334d2fcbd4606f98abc92344a923f685d
vinimoraes <viniciusmoraesvha@gmail.com>
2025-03-25 17:59:37 -0400

    Add random number generation function and remove commented-out code

--- aula-1/src/main.ts
+++ aula-1/src/main.ts
@@ -1,8 +1,26 @@
-let alfabeto = "abcdefghijklmnopqrstuvwxyz"
-let palavra = "acerolaz"
+// let alfabeto = "abcdefghijklmnopqrstuvwxyz"
+// let palavra = "acerolaz"
 
-for(let letra of palavra) {
-    let posicao = alfabeto.indexOf(letra)
-    let cifrado = alfabeto[(posicao + 1) % alfabeto.length]
-    console.log(`[${posicao}]`, letra, '-->', cifrado)
+// for(let letra of palavra) {
+//     let posicao = alfabeto.indexOf(letra)
+//     let cifrado = alfabeto[(posicao + 1) % alfabeto.length]
+//     console.log(`[${posicao}]`, letra, '-->', cifrado)
+// }
+
+// Random Numbers
+
+function randomNumbers(x: number, y: number, z: number) {
+    let arr: number[] = []
+    
+    while(arr.length < x) {
+        let newNumber: number = Math.ceil(Math.random() * z)
+
+        if(!(newNumber < y || newNumber > z)) {
+            arr.push(newNumber)
+        } 
+    }
+
+    console.log(arr)
 }
+
+randomNumbers(100, 1, 1000)
