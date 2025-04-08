7b95172cd8cd4f0a2036b741c986c61cea7fcbdd
Vinícius Moraes <viniciusmoraesvha@gmail.com>
2025-04-07 22:17:50 -0400

    Add initial implementation of a stack-based expression evaluator in index.ts

---
+++ pilha/src/index.ts
@@ -0,0 +1,40 @@
+const expression = "2 7 + 9 + 4 / 6 *"
+
+const splittedExpression = expression.split(" ")
+
+const stack = []
+
+for (let element of splittedExpression) {
+    if (element !== "+" && element !== "-" && element !== "/" && element !== "*") {
+        stack.push(parseFloat(element))
+    } else {
+        let operator = element
+        let result: number = 0
+
+        let a = stack.pop()
+        let b = stack.pop()
+
+        if (a == undefined || b == undefined) {
+            throw new Error("erro")
+        }
+
+        switch (operator) {
+            case "+":
+                result = b + a
+                break
+            case "-":
+                result = b - a
+                break
+            case "/":
+                result = b / a
+                break
+            case "*":
+                result = b * a
+                break
+        }
+
+        stack.push(result)
+    }
+}
+
+console.log(`Resultado: ${stack}`);
