d90cf00b30368e0cd3e214ee0bc312bd829fba55
Vinícius Moraes <viniciusmoraesvha@gmail.com>
2025-05-03 15:59:17 -0400

    Implement deque functionality with enqueue and dequeue operations in index.ts

--- dequeue/src/index.ts
+++ dequeue/src/index.ts
@@ -1,1 +1,16 @@
-console.log("Hello, world!");
+import { Deque } from "./Deque";
+
+const deque = new Deque(5);
+
+for(let i = 5; i >= 0; i--) {
+    console.log("Inserido A");
+    deque.enqueue("A");
+    console.log("Inserido B");
+    deque.enqueue("B");
+    console.log("Inserido C");
+    deque.enqueue("C");
+
+    console.log("Removido:", deque.dequeue());
+    console.log("Removido:", deque.dequeue());
+    console.log("Removido:", deque.dequeue());
+}
