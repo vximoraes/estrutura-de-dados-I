1a91b435d38b9b5703381013f7a26232a087251b
Vinícius Moraes <viniciusmoraesvha@gmail.com>
2025-05-05 17:28:54 -0400

    feat: add initial implementation of index.ts for Deque operations

---
+++ deque/src/index.ts
@@ -0,0 +1,18 @@
+import { Deque } from "./Deque";
+
+const deque = new Deque(7);
+
+deque.addfirst("C");
+console.log(deque);
+deque.addfirst("D");
+console.log(deque);
+deque.addfirst("E");
+console.log(deque);
+deque.addfirst("C");
+console.log(deque);
+deque.addfirst("D");
+console.log(deque);
+deque.addfirst("E");
+console.log(deque);
+deque.addfirst("E");
+console.log(deque);
