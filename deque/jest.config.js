d3511ba616deb40a3f717e13d4bdf4fb34beea5d
vinimoraes <viniciusmoraesvha@gmail.com>
2025-05-07 16:16:39 -0400

    chore: format jest.config.js for improved readability

--- deque/jest.config.js
+++ deque/jest.config.js
@@ -1,14 +1,14 @@
 export default {
-  preset: "ts-jest/presets/default-esm",
-  testEnvironment: "node",
-  transform: {
-    "^.+\\.tsx?$": "ts-jest",
-  },
-  moduleFileExtensions: ["ts", "js"],
-  extensionsToTreatAsEsm: [".ts"],
-  globals: {
-    "ts-jest": {
-      useESM: true,
+    preset: "ts-jest/presets/default-esm",
+    testEnvironment: "node",
+    transform: {
+        "^.+\\.tsx?$": "ts-jest",
     },
-  },
+    moduleFileExtensions: ["ts", "js"],
+    extensionsToTreatAsEsm: [".ts"],
+    globals: {
+        "ts-jest": {
+            useESM: true,
+        },
+    },
 };
